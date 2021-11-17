3d974ed60f6b91299d67729c87790c52d206902f
HamadaTomoki <hamada.tomoki01@gmail.com>
2021-11-17 22:51:43 +0900

    [Update] State hoisting(状態の巻き上げ) について学習

--- BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,21 +3,19 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,16 +28,45 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+/** Composable ------------------------------------------------------------- */
+
 @Composable
-private fun Myapp(names: List<String> = listOf("World", "Compose")) {
-    Surface(
-        color = MaterialTheme.colors.background,
-        modifier = Modifier.padding(vertical = 4.dp)
-    ) {
-        Column {
-            for (name in names) {
-                Greeting(name = name)
+private fun Myapp() {
+    /** State hoisting */
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(), // 余白を最大まで要素で埋める
+            verticalArrangement = Arrangement.Center, // 縦方向で中央に寄せる
+            horizontalAlignment = Alignment.CenterHorizontally // 横方向で中央に寄せる
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
             }
+        }
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
         }
     }
 }
@@ -73,6 +100,9 @@ fun Greeting(name: String) {
     }
 }
 
+
+/** Preview ------------------------------------------------------------- */
+
 @Preview(
     showBackground = true,
     name = "Text preview", // プレビュー名
@@ -82,5 +112,14 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     BasicsCodelabTheme {
         Myapp()
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {}) // プレビューには空のラムダを渡す
     }
 }
