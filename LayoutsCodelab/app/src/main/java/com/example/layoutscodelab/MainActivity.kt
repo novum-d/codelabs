9dca00aa99e6de79d8d601bb106f90872276b22b
HamadaTomoki <hamada.tomoki01@gmail.com>
2021-11-20 20:04:47 +0900

    [Update] I learned how to create Custom Layout

--- LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/MainActivity.kt
+++ LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/MainActivity.kt
@@ -13,15 +13,15 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.RemeasurementModifier
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.layoutscodelab.ui.theme.LayoutsCodelabTheme
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                ScrollingList()
+                LayoutsCodelab()
             }
         }
     }
@@ -40,6 +40,30 @@ class MainActivity : ComponentActivity() {
 
 
 /** Composable ------------------------------------------------------------- */
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit // custom layout attributes
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
 
 @Composable
 fun ScrollingList() { // the coroutine make scrolling smoothly(rendering will no longer be blocked)
@@ -148,9 +172,11 @@ fun LayoutsCodelab() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
 }
 
@@ -185,7 +211,45 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
     }
 }
 
+/** Modifier ------------------------------------------------------------- */
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints) // measure composable
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+
 /** Preview ------------------------------------------------------------- */
+
+@Preview
+@Composable
+fun TextWidthPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWidthNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there", Modifier.padding(32.dp))
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
