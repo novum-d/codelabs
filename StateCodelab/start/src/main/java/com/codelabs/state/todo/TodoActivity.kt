ad385145ed0a8f7a8fa5fbd10b638f2f77aee15f
HamadaTomoki <hamada.tomoki01@gmail.com>
2021-11-27 02:31:41 +0900

    [Update] Learned unidirectional data flow and viewmodel in jetpack compose

--- StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
+++ StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.codelabs.state.todo
 
 import android.os.Bundle
@@ -21,20 +5,38 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import com.codelabs.state.ui.StateCodelabTheme
+
 
 class TodoActivity : AppCompatActivity() {
 
-    val todoViewModel by viewModels<TodoViewModel>()
+    private val todoViewModel by viewModels<TodoViewModel>() // up
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StateCodelabTheme {
                 Surface {
-                    // TODO: build the screen in compose
+                    TodoActivityScreen(todoViewModel)
                 }
             }
         }
     }
 }
+
+@Composable
+private fun TodoActivityScreen(todoViewModel: TodoViewModel) { // down
+    // observeAsState observes a LiveData and returns a State object that is updated whenever the LiveData changes.
+    val items: List<TodoItem> by todoViewModel.todoItems.observeAsState(listOf())
+    TodoScreen(
+        // state
+        items = items,
+        // event(the method reference syntax)
+        onAddItem = todoViewModel::addItem,
+        onRemoveItem = todoViewModel::removeItem
+    )
+}
+
