331e2983a799074284311df0cfb32c8803ace033
HamadaTomoki <hamada.tomoki01@gmail.com>
2021-12-27 00:37:43 +0900

    :sparkles: Update ThemingCodelab.

--- ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -54,7 +54,8 @@ import androidx.compose.ui.unit.dp
 import com.codelab.theming.R
 import com.codelab.theming.data.Post
 import com.codelab.theming.data.PostRepo
-import java.util.Locale
+import com.codelab.theming.ui.start.theme.JetnewsTheme
+import java.util.*
 
 @Composable
 fun Home() {
@@ -217,7 +218,18 @@ private fun PostItemPreview() {
 @Composable
 private fun FeaturedPostPreview() {
     val post = remember { PostRepo.getFeaturedPost() }
-    FeaturedPost(post = post)
+    JetnewsTheme {
+        FeaturedPost(post = post)
+    }
+}
+
+@Preview("Featured Post • Dark")
+@Composable
+private fun FeaturedPostDarkPreview() {
+    val post = remember { PostRepo.getFeaturedPost() }
+    JetnewsTheme(darkTheme = true) {
+        FeaturedPost(post = post)
+    }
 }
 
 @Preview("Home")
