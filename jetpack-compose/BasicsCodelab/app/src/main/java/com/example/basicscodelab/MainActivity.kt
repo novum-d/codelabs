546919b89e462e93aa647b33348898b85afb5879
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-10-03 02:34:50 +0900

    :tada: Done jetpack compose basic in codelab.

---
+++ jetpack-compose/BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.basicscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import com.example.basicscodelab.ui.screen.Greetings
+import com.example.basicscodelab.ui.screen.OnboardingScreen
+import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboardingScreen { shouldShowOnboarding = false }
+    } else {
+        Greetings()
+    }
+}
